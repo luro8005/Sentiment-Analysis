120f3db2338551cc2156f67c745cc85374722497
luis <luro8005@colorado.edu>
2018-09-27 15:51:58 -0600

    testing tablesaw

--- src/main/java/hackathon/App.java
+++ src/main/java/hackathon/App.java
@@ -1,5 +1,7 @@
 package hackathon;
 
+import tech.tablesaw.api.DoubleColumn;
+
 public class App {
     public static void main(String[] args) {
         String text = "I don't like the #Spectrum app";
@@ -7,6 +9,15 @@ public class App {
         Sentiment.getSentiment(text);
 
         System.out.println("Done.");
+
+        System.out.println();
+
+        double[] numbers = {1, 2, 3, 4};
+        DoubleColumn nc = DoubleColumn.create("Test", numbers);
+        System.out.println(nc.print());
+
+        DoubleColumn nc2 = nc.multiply(7);
+        System.out.println(nc2.print());
 
     }
 
---
+++ src/main/java/hackathon/dataVisualization/SampleData.java
@@ -0,0 +1,10 @@
+package hackathon.dataVisualization;
+
+import tech.tablesaw.api.DoubleColumn;
+
+public class SampleData {
+
+    double[] numbers = {1, 2, 3, 4};
+    DoubleColumn nc = DoubleColumn.create("Test", numbers);
+
+}
