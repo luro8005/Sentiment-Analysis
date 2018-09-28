59be16afebbf88dbe2be9b6d96f3a9c67a86158c
Joe Kendall <joe.kendall@charter.com>
2018-09-28 09:36:46 -0600

    added RS

---
+++ src/main/java/hackathon/rest/SentimentRS.java
@@ -0,0 +1,12 @@
+package hackathon.rest;
+/**
+ * Copyright 2018, Charter Communications,  All rights reserved.
+ */
+
+/**
+ * @author jkendall1
+ */
+@RestController
+@RequestMapping("/account")
+public class SentimentRS {
+}
